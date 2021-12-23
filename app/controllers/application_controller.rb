02fad35c8bfc2613634aa66f6b6ebf7d5b73459f
Mel <melanievillalobos016ags@gmail.com>
2021-12-23 13:44:06 -0600

    blog finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    def index
+        @articles = Articles.all
+    end
 end
